e8789da411483b5dd7cdd869c2bce14ecffc0645
Nicoplv <nicoplv@outlook.com>
2015-01-24 17:36:46 +0100

    Add TileManager

---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// ----------
+	// VARIABLE
+	// ----------
+
+	public bool available = true;
+
+	public bool right = false;
+	public bool left = false;
+	public bool up = false;
+	public bool down = false;
+
+
+	// ----------
+	// UNITY
+	// ----------
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+
+	}
+
+
+	// ----------
+	// UTILITIES
+	// ----------
+
+}
